5c4e1e35e8315661e33e9fc4290f116f757ce06a
John Warwick <jwarwick@gmail.com>
2018-12-08 13:23:17 -0500

    Day 8, part 2

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -2,7 +2,6 @@ extern crate util;
 extern crate rand;
 
 use rand::prelude::random;
-use std::collections::HashSet;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 
@@ -13,12 +12,35 @@ type Tree = HashMap<NodeId, Node>;
 struct Node {
     id: NodeId,
     parent: Option<NodeId>,
-    children: HashSet<NodeId>,
+    children: Vec<NodeId>,
     metadata: Vec<i32>,
 }
 
+impl Node {
+    fn compute_value(&self, tree: &Tree) -> i32 {
+        if self.children.is_empty() {
+            self.metadata.iter().sum()
+        } else {
+            let mut total = 0;
+            for v in self.metadata.clone() {
+                let child_id = self.children.get((v as usize) - 1);
+                total +=
+                    match child_id {
+                        None => 0,
+                        Some(x) =>
+                        {
+                            let n: &Node = tree.get(&x).unwrap();
+                            n.compute_value(tree)
+                        },
+                    }
+            }
+            total
+        }
+    }
+}
+
 pub fn metadata_sum(s: &str) -> i32 {
-    let tree = build_tree(&s);
+    let (_root_id, tree) = build_tree(&s);
     let mut total = 0;
     for n in tree.values() {
         let s: i32 = n.metadata.iter().sum();
@@ -27,11 +49,17 @@ pub fn metadata_sum(s: &str) -> i32 {
     total
 }
 
-fn build_tree(s: &str) -> Tree {
+pub fn compute_root_value(s: &str) -> i32 {
+    let (root_id, tree) = build_tree(&s);
+    let root = tree.get(&root_id).unwrap();
+    root.compute_value(&tree)
+}
+
+fn build_tree(s: &str) -> (NodeId, Tree) {
     let mut nums: VecDeque<NodeId> = s.split_whitespace().flat_map(|x| x.parse()).collect();
     let mut tree: Tree = HashMap::new();
-    add_node(None, &mut tree, &mut nums);
-    tree
+    let root_id = add_node(None, &mut tree, &mut nums);
+    (root_id, tree)
 }
 
 fn add_node(parent_id: Option<NodeId>, tree: &mut Tree, nums: &mut VecDeque<NodeId>) -> NodeId {
@@ -41,12 +69,12 @@ fn add_node(parent_id: Option<NodeId>, tree: &mut Tree, nums: &mut VecDeque<Node
     let mut node = Node {
         id: node_id.clone(),
         parent: parent_id,
-        children: HashSet::new(),
+        children: Vec::new(),
         metadata: Vec::new(),
     };
     for _ in 0..child_cnt {
         let child_id = add_node(node.parent, tree, nums);
-        node.children.insert(child_id);
+        node.children.push(child_id);
     }
     node.metadata = nums.iter().take(meta_cnt).cloned().collect();
     nums.drain(..meta_cnt);
@@ -68,5 +96,23 @@ mod tests {
     fn part1_example() {
         let input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
         assert_eq!(metadata_sum(&input), 138);
+    }
+
+    #[test]
+    fn one_node_value() {
+        let input = "0 3 10 11 12";
+        assert_eq!(compute_root_value(&input), 33);
+    }
+
+    #[test]
+    fn no_child_value() {
+        let input = "1 1 0 1 99 2";
+        assert_eq!(compute_root_value(&input), 0);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+        assert_eq!(compute_root_value(&input), 66);
     }
 }
--- day08/src/main.rs
+++ day08/src/main.rs
@@ -10,6 +10,6 @@ fn main() {
     let result1 = day08::metadata_sum(&contents);
     println!("Part 1 Result: {}", result1);
 
-    //let result2 = day07::parallel_ordering(&contents, 5, 60);
-    //println!("Part 2 Result: {}", result2);
+    let result2 = day08::compute_root_value(&contents);
+    println!("Part 2 Result: {}", result2);
 }
